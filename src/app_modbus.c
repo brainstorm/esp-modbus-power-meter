222ebaae5be7bba6539c1539e87db10d73eba6fb
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2022-03-22 23:05:20 +0000

    During this code cleanup, I realise that I should be implementing a **master**, not a **slave**... :facepalm:

--- src/app_modbus.c
+++ src/app_modbus.c
@@ -24,24 +24,13 @@ static const char *TAG = "app_modbus";
 
 // Defines below are used to define register start address for each type of Modbus registers
 #define HOLD_OFFSET(field) ((uint16_t)(offsetof(holding_reg_params_t, field) >> 1))
-#define INPUT_OFFSET(field) ((uint16_t)(offsetof(input_reg_params_t, field) >> 1))
-#define MB_REG_DISCRETE_INPUT_START         (0x0000)
-#define MB_REG_COILS_START                  (0x0000)
-#define MB_REG_INPUT_START_AREA0            (INPUT_OFFSET(input_data0)) // register offset input area 0
-#define MB_REG_INPUT_START_AREA1            (INPUT_OFFSET(input_data4)) // register offset input area 1
 #define MB_REG_HOLDING_START_AREA0          (HOLD_OFFSET(holding_data0))
 #define MB_REG_HOLDING_START_AREA1          (HOLD_OFFSET(holding_data4))
 
-#define MB_PAR_INFO_GET_TOUT                (10) // Timeout for get parameter info
+#define MB_PAR_INFO_GET_TIMEOUT             10 // Timeout for get parameter info
 #define MB_CHAN_DATA_MAX_VAL                (6)
 #define MB_CHAN_DATA_OFFSET                 (0.2f)
-#define MB_READ_MASK                        (MB_EVENT_INPUT_REG_RD \
-                                                | MB_EVENT_HOLDING_REG_RD \
-                                                | MB_EVENT_DISCRETE_RD \
-                                                | MB_EVENT_COILS_RD)
-#define MB_WRITE_MASK                       (MB_EVENT_HOLDING_REG_WR \
-                                                | MB_EVENT_COILS_WR)
-#define MB_READ_WRITE_MASK                  (MB_READ_MASK | MB_WRITE_MASK)
+#define MB_READ_MASK                        (MB_EVENT_HOLDING_REG_RD)
 
 #define MB_PORT_NUM     UART_NUM_1   // Number of UART port used for Modbus connection
 //#define MB_SLAVE_ADDR   (CONFIG_FMB_CONTROLLER_SLAVE_ID)      // The address of device in Modbus network
@@ -50,6 +39,14 @@ static portMUX_TYPE param_lock = portMUX_INITIALIZER_UNLOCKED;
 
 static void app_modbus_update(TimerHandle_t handle)
 {
+
+    // Initialization of Discrete Inputs register area
+    reg_area.type = MB_REG_HOLDING_START_AREA0;
+    reg_area.start_offset = MB_REG_DISCRETE_INPUT_START;
+    reg_area.address = (void*)&discrete_reg_params;
+    reg_area.size = sizeof(discrete_reg_params);
+    ESP_ERROR_CHECK(mbc_slave_set_descriptor(reg_area));
+
     // TODO: Fetch current power
 
     // esp_rmaker_param_update_and_report(
@@ -63,9 +60,6 @@ esp_err_t freemodbus_init(void)
     // Here are the user defined instances for device parameters packed by 1 byte
     // These are keep the values that can be accessed from Modbus master
     holding_reg_params_t holding_reg_params = { 0 };
-    input_reg_params_t input_reg_params = { 0 };
-    coil_reg_params_t coil_reg_params = { 0 };
-    discrete_reg_params_t discrete_reg_params = { 0 };
 
     mb_param_info_t reg_info; // keeps the Modbus registers access information
     mb_communication_info_t comm_info; // Modbus communication parameters
@@ -80,22 +74,17 @@ esp_err_t freemodbus_init(void)
     }
 
     comm_info.mode = MB_MODE_RTU,
-
-    comm_info.slave_addr = 1;
+    comm_info.slave_addr = 2;
     comm_info.port = MB_PORT_NUM;
     comm_info.baudrate = 9600;
     comm_info.parity = MB_PARITY_NONE;
     ESP_ERROR_CHECK(mbc_slave_setup((void*)&comm_info));
 
-    // The code below initializes Modbus register area descriptors
-    // for Modbus Holding Registers, Input Registers, Coils and Discrete Inputs
-    // Initialization should be done for each supported Modbus register area according to register map.
-    // When external master trying to access the register in the area that is not initialized
-    // by mbc_slave_set_descriptor() API call then Modbus stack
-    // will send exception response for this register area.
+    // Initialize Modbus register area descriptors
     reg_area.type = MB_PARAM_HOLDING; // Set type of register area
     reg_area.start_offset = MB_REG_HOLDING_START_AREA0; // Offset of register area in Modbus protocol
     reg_area.address = (void*)&holding_reg_params.holding_data0; // Set pointer to storage instance
+    
     // Set the size of register storage instance = 150 holding registers
     reg_area.size = (size_t)(HOLD_OFFSET(holding_data4) - HOLD_OFFSET(test_regs));
     ESP_ERROR_CHECK(mbc_slave_set_descriptor(reg_area));
@@ -103,32 +92,6 @@ esp_err_t freemodbus_init(void)
     reg_area.start_offset = MB_REG_HOLDING_START_AREA1; // Offset of register area in Modbus protocol
     reg_area.address = (void*)&holding_reg_params.holding_data4; // Set pointer to storage instance
     reg_area.size = sizeof(float) << 2; // Set the size of register storage instance
-    ESP_ERROR_CHECK(mbc_slave_set_descriptor(reg_area));
-
-    // Initialization of Input Registers area
-    reg_area.type = MB_PARAM_INPUT;
-    reg_area.start_offset = MB_REG_INPUT_START_AREA0;
-    reg_area.address = (void*)&input_reg_params.input_data0;
-    reg_area.size = sizeof(float) << 2;
-    ESP_ERROR_CHECK(mbc_slave_set_descriptor(reg_area));
-    reg_area.type = MB_PARAM_INPUT;
-    reg_area.start_offset = MB_REG_INPUT_START_AREA1;
-    reg_area.address = (void*)&input_reg_params.input_data4;
-    reg_area.size = sizeof(float) << 2;
-    ESP_ERROR_CHECK(mbc_slave_set_descriptor(reg_area));
-
-    // Initialization of Coils register area
-    reg_area.type = MB_PARAM_COIL;
-    reg_area.start_offset = MB_REG_COILS_START;
-    reg_area.address = (void*)&coil_reg_params;
-    reg_area.size = sizeof(coil_reg_params);
-    ESP_ERROR_CHECK(mbc_slave_set_descriptor(reg_area));
-
-    // Initialization of Discrete Inputs register area
-    reg_area.type = MB_PARAM_DISCRETE;
-    reg_area.start_offset = MB_REG_DISCRETE_INPUT_START;
-    reg_area.address = (void*)&discrete_reg_params;
-    reg_area.size = sizeof(discrete_reg_params);
     ESP_ERROR_CHECK(mbc_slave_set_descriptor(reg_area));
 
     //setup_reg_data(); // Set values into known state
@@ -141,37 +104,7 @@ esp_err_t freemodbus_init(void)
                             CONFIG_MB_UART_RXD, CONFIG_MB_UART_RTS,
                             UART_PIN_NO_CHANGE));
 
-    ESP_LOGI(TAG, "Modbus slave stack initialized.");
-
-    // The cycle below will be terminated when parameter holdingRegParams.dataChan0
-    // incremented each access cycle reaches the CHAN_DATA_MAX_VAL value.
-    for(;holding_reg_params.holding_data0 < MB_CHAN_DATA_MAX_VAL;) {
-        // Check for read/write events of Modbus master for certain events
-        mb_event_group_t event = mbc_slave_check_event(MB_READ_WRITE_MASK);
-        const char* rw_str = (event & MB_READ_MASK) ? "READ" : "WRITE";
-
-        // Filter events and process them accordingly
-        if(event & (MB_EVENT_HOLDING_REG_WR | MB_EVENT_HOLDING_REG_RD)) {
-            // Get parameter information from parameter queue
-            ESP_ERROR_CHECK(mbc_slave_get_param_info(&reg_info, MB_PAR_INFO_GET_TOUT));
-            ESP_LOGI(TAG, "HOLDING %s (%u us), ADDR:%u, TYPE:%u, INST_ADDR:0x%.4x, SIZE:%u",
-                    rw_str,
-                    (uint32_t)reg_info.time_stamp,
-                    (uint32_t)reg_info.mb_offset,
-                    (uint32_t)reg_info.type,
-                    (uint32_t)reg_info.address,
-                    (uint32_t)reg_info.size);
-            if (reg_info.address == (uint8_t*)&holding_reg_params.holding_data0)
-            {
-                portENTER_CRITICAL(&param_lock);
-                holding_reg_params.holding_data0 += MB_CHAN_DATA_OFFSET;
-                if (holding_reg_params.holding_data0 >= (MB_CHAN_DATA_MAX_VAL - MB_CHAN_DATA_OFFSET)) {
-                    coil_reg_params.coils_port1 = 0xFF;
-                }
-                portEXIT_CRITICAL(&param_lock);
-            }
-        }
-    }
+    ESP_LOGI(TAG, "Modbus slave stack initialized.");    
 
     return ESP_OK;
 }
