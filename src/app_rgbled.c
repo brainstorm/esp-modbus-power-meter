c29fdab9033b4c7e389a4397fc3379a5f804e426
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2022-03-25 23:17:35 +0000

    Reduce rgb led code to almost nothing, reducing complexity of it for now

--- src/app_rgbled.c
+++ src/app_rgbled.c
@@ -1,38 +1,22 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/timers.h>
 #include <sdkconfig.h>
-#include <esp_rmaker_core.h>
-#include <esp_rmaker_standard_types.h> 
-#include <esp_rmaker_standard_params.h> 
 
 #include <app_reset.h>
 #include <ws2812_led.h>
-#include "app_priv.h"
-
-/* This is the button that is used for toggling the power */
-#define BUTTON_GPIO          0
-#define BUTTON_ACTIVE_LEVEL  0
-/* This is the GPIO on which the power will be set */
-#define OUTPUT_GPIO    19
+// #include "app_priv.h"
 
 #define DEFAULT_SATURATION  100
 #define DEFAULT_BRIGHTNESS  50
 #define DEFAULT_HUE 90
 
-#define WIFI_RESET_BUTTON_TIMEOUT       3
-#define FACTORY_RESET_BUTTON_TIMEOUT    10
-
 static uint16_t g_hue = DEFAULT_HUE;
 static uint16_t g_saturation = DEFAULT_SATURATION;
 static uint16_t g_value = DEFAULT_BRIGHTNESS;
-static float g_temperature;
 
 static void app_rgbled_update()
 {
     ws2812_led_set_hsv(DEFAULT_HUE, g_saturation, g_value);
-    esp_rmaker_param_update_and_report(
-            esp_rmaker_device_get_param_by_type(power_sensor_device, ESP_RMAKER_PARAM_TEMPERATURE),
-            esp_rmaker_float(g_temperature));
 }
 
 esp_err_t rgbled_init(void)
@@ -42,22 +26,13 @@ esp_err_t rgbled_init(void)
         return err;
     }
 
-    //g_temperature = DEFAULT_TEMPERATURE;
-    //sensor_timer = xTimerCreate("app_rgbled_update_tm", (REPORTING_PERIOD * 1000) / portTICK_PERIOD_MS,
-    //                        pdTRUE, NULL, app_rgbled_update);
-    // (sensor_timer) {
-    // if   xTimerStart(sensor_timer, 0);
-    //    g_hue = (100 - g_temperature) * 2;
+    // TODO: Do something more fancy here and perhaps related with modbus/status.
     ws2812_led_set_hsv(g_hue, g_saturation, g_value);
     return ESP_OK;
-    //}
-    //return ESP_FAIL;
 }
 
 void app_rgbled_init()
 {
     rgbled_init();
-    app_reset_button_register(app_reset_button_create(BUTTON_GPIO, BUTTON_ACTIVE_LEVEL),
-                WIFI_RESET_BUTTON_TIMEOUT, FACTORY_RESET_BUTTON_TIMEOUT);
     app_rgbled_update();
 }
